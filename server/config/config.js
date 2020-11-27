95282b5f26b2ea8635648247f17f4c6acbf88507
Miguel Angel Dulcey Diaz <mpgigat@gmail.com>
2020-11-27 15:41:57 -0500

    Restserver - usuario3

--- server/config/config.js
+++ server/config/config.js
@@ -8,10 +8,10 @@ process.env.NODE_ENV=process.NODE_ENV || 'dev';
 
 let urlDB;
 
-if(process.env.NODE_ENV==='dev'){
-    urlDB='mongodb://localhost:27017/cafe';
-}else{
-      urlDB='mongodb+srv://admin:CvNgd32uWQ1ih9Mc@cluster0.zxyca.mongodb.net/sub-inv'
-}
+// if(process.env.NODE_ENV==='dev'){
+//     urlDB='mongodb://localhost:27017/cafe';
+// }else{
+       urlDB='mongodb+srv://admin:CvNgd32uWQ1ih9Mc@cluster0.zxyca.mongodb.net/sub-inv'
+// }
 
 process.env.URLDB=urlDB;
