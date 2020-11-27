f86b6cf2001f14aebcd3be42676f7a45ed778ff4
Miguel Angel Dulcey Diaz <mpgigat@gmail.com>
2020-11-27 16:19:54 -0500

    Restserver - variables de entonorno mongo2

--- server/config/config.js
+++ server/config/config.js
@@ -12,8 +12,7 @@ if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
     //urlDB='mongodb+srv://admin:CvNgd32uWQ1ih9Mc@cluster0.zxyca.mongodb.net/sub-inv'
-    urlDB=process.env.MONGO_URI
-
+    urlDB = process.env.MONGO_URI;
 }
 console.log(urlDB);
 process.env.URLDB=urlDB;
